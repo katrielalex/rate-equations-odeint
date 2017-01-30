20c03b0dce090e9724c81b74df494613cfa5bcff
Katriel Cohn-Gordon <katriel.cohn-gordon@cybersecurity.ox.ac.uk>
2017-01-30 19:41:03 +0000

    fiddle with rates, now bad

--- rate_equations.py
+++ rate_equations.py
@@ -15,13 +15,13 @@ from scipy.integrate import odeint
 X = 0.5
 
 # Reaction rates are b, c, d
-rates = (0.1, 0.2, 0.3)
+rates = (0.1, 0.4, 0.3)
 
 # Take statespace as: (B,C,D,E,F,G)
 initial_conditions = (0.1, 0, 0, 1, 1.5, 0)
 
-# Time points to solve for concentrations; let's use 100 steps from 0 to 10
-time_points = np.linspace(0, 10, 100)
+# Time points to solve for concentrations
+time_points = np.linspace(0, 10, 10000)
 
 
 # Given particular reaction rates, solve for the concentrations of each reagent over time.
@@ -35,15 +35,16 @@ def concentrations(rates):
         D_prime = -F_prime - G_prime
         E_prime = -B_prime - G_prime
         return (B_prime, C_prime, D_prime, E_prime, F_prime, G_prime)
-
     return odeint(d_by_dt, initial_conditions, time_points, rates)
 
+
 if __name__ == "__main__":
-        parser = argparse.ArgumentParser(description="Do some rate equation computations")
-        args = parser.parse_args()
+    parser = argparse.ArgumentParser(description="Do some rate equation computations")
+    parser.add_argument("--rates", help="coefficients for b, c, d")
+    args = parser.parse_args()
 
-        logging.basicConfig(level=logging.DEBUG)
-
-        solution = pd.DataFrame(concentrations(rates), columns=list('BCDEFG'))
-        solution.plot()
-        plt.show()
+    logging.basicConfig(level=logging.DEBUG)
+    rates = tuple(float(rate) for rate in args.rates.split(",")) if args.rates else rates
+    solution = pd.DataFrame(concentrations(rates), columns=list('BCDEFG'))
+    solution.plot()
+    plt.show()
